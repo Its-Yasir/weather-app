4fa48150a3a57a8b7372487abbe62bd795098564
Its-Yasir <iamyasirtariq@gmail.com>
2025-08-12 10:30:13 +0500

    Added grids

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
-const url = 'https://weatherapi-com.p.rapidapi.com/alerts.json?q=hafizabad';
+const url = 'https://api.weatherapi.com/v1/current.json?key=b19bdbbf07a84cba965153941251108&q=hafizabad';
 const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': '07b0e29c2bmsh10e4ce27b95ed92p17cdbbjsnf5dbb63b6166',
-		'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com'
-	}
+  method: 'GET'
 };
 
-
-try {
-	const response = await fetch(url, options);
-	const result = JSON.parse(await response.text());
-	console.log(result);
-} catch (error) {
-	console.error(error);
+async function fetchWeather() {
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error('Error fetching weather data:', error);
+  }
 }
 
-const aurl = 'https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?lat=32.5667&lon=74.0833&units=metric&lang=en';
-const aoptions = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': '07b0e29c2bmsh10e4ce27b95ed92p17cdbbjsnf5dbb63b6166',
-		'x-rapidapi-host': 'weatherbit-v1-mashape.p.rapidapi.com'
-	}
-};
+fetchWeather();
 
-try {
-	const aresponse = await fetch(aurl, aoptions);
-	const aresult = JSON.parse(await aresponse.text());
-	console.log(aresult);
-} catch (error) {
-	console.error(error);
-}
+// const aurl = 'https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?lat=32.5667&lon=74.0833&units=metric&lang=en';
+// const aoptions = {
+// 	method: 'GET',
+// 	headers: {
+// 		'x-rapidapi-key': '07b0e29c2bmsh10e4ce27b95ed92p17cdbbjsnf5dbb63b6166',
+// 		'x-rapidapi-host': 'weatherbit-v1-mashape.p.rapidapi.com'
+// 	}
+// };
+
+// try {
+// 	const aresponse = await fetch(aurl, aoptions);
+// 	const aresult = JSON.parse(await aresponse.text());
+// 	console.log(aresult);
+// } catch (error) {
+// 	console.error(error);
+// }
